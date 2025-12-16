c68bb17829e708024ac96524a4ca60a45176213b
namanjainba-source <namanjain.ba@gmail.com>
2025-12-16 11:56:58 +0530

    Add files via upload

---
+++ frontend/next.config.js
@@ -0,0 +1,5 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', 
+};
+module.exports = nextConfig;
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+﻿import type { Config } from "tailwindcss";
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: { 'progress': 'progress 2s ease-in-out infinite' },
+      keyframes: {
+        progress: {
+          '0%': { transform: 'translateX(-100%)' },
+          '50%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
